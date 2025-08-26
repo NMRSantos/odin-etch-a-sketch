0f901b687ee45f8b86a8212705deaa8e60447571
NMRSantos <170880035+NMRSantos@users.noreply.github.com>
2025-08-26 19:59:47 +0100

    Update logic and add grid size change button

--- scripts.js
+++ scripts.js
@@ -1,42 +1,51 @@
-const body = document.getElementsByName(".body");
-const row = document.createElement("div");
-document.body.appendChild(row);
-row.style.display = "flex";
-row.style.width = "525px";
-row.style.padding = "0px";
-row.style.margin = "0px";
-row.style.flexWrap = "wrap";
+const grid = document.querySelector(".grid");
+const button = document.querySelector(".button");
 
+let gridSize = 16
 
-let gridSize = 16;
+function makeSquare(row) {
+    const square = document.createElement("div");
+    //SQUARE CSS STYLING
+    row.appendChild(square);
+    square.style.height = "16px";
+    square.style.width = "16px";
+    square.style.border = "solid black 1px";
+    square.className = "square";
+};
 
 function makeRow() {
+    const row = document.createElement("div");
+    row.className = "row";
+    grid.appendChild(row);
     for (let i = 0; i < gridSize; i++) {
-        const cube = document.createElement("div");
-        row.appendChild(cube);
-        cube.style.height = "30px";
-        cube.style.width = "30px";
-        cube.style.border = "solid, black, 1px";        
-    };
+        makeSquare(row); 
+    }
 };
 
 function makeGrid() {
-    for (let i = 0; i < gridSize; i++)
-        makeRow();
-    stopPropagation()
+    for (let i = 0; i < gridSize; i++) {
+        makeRow(); 
+    }
 };
 
-addEventListener("mouseover", (event) => {
-    event.target.style.backgroundColor = "red";
+document.addEventListener("mouseover", (event) => {
+    if(event.target.classList.contains("square")) {
+        event.target.style.backgroundColor = "red";
+    };
 });
-
 
 makeGrid();
 
-
-// make a 16x16 grid
-//make one row with 16 boxes
-//repeat until 16 rows
-
-
-// make one box
+button.addEventListener("click", () => {
+    gridSize = prompt("Please, set a grid size");
+    if (gridSize <= 100) {
+        grid.innerHTML = "";
+        makeGrid();
+        console.log("New grid size is", gridSize);
+    } else {
+        gridSize = prompt("Please, set a size lower than 100");
+        grid.innerHTML = "";
+        makeGrid();
+        console.log("New grid size is", gridSize);
+    };
+});
